7b442e2659fbbb40eea9bd88f6728face0d37d04
ThomasOberlin <tomsoberlin@gmail.com>
2025-08-28 15:38:36 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ai-grocery-roi-calculator/',
-  build: {
-    outDir: 'dist',
-  },
+  base: '/ai-grocery-roi-calculator-v2/'
 })
